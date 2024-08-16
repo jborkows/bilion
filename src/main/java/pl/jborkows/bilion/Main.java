b06f4aa4993be24c0077026259b0ffa55f24a883
jborkows <jacek.borkowskiit@gmail.com>
2024-08-16 10:14:36 +0200

    whole one...

--- src/main/java/pl/jborkows/bilion/Main.java
+++ src/main/java/pl/jborkows/bilion/Main.java
@@ -15,12 +15,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         var path = extractPath(args);
         List<Runner> runners = List.of(
-//                new Simple(), //-->base
-//                new OwnSplit(),
-//                new OwnSplitStringGetters()
-//                new OwnSplitDoubleParser(),
+                new Simple(), //-->base
+                new OwnSplit(),
+                new OwnSplitStringGetters(),
+                new OwnSplitDoubleParser(),
                 new OwnSplitDoubleActiveParser(),
-//                new OwnSplitDoubleActiveParserStaticWorkingArray(),
+                new OwnSplitDoubleActiveParserStaticWorkingArray(),
                 new OwnSplitDoubleActiveParserIndexBased()
         );
 
